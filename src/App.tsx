553237c066f8de9b3ae14f469ecf8ec03c89bb72
Jaeeun <jaeeun9862@gmail.com>
2023-11-07 11:34:57 +0900

    라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './routes/Home';
+import Profile from './routes/Profile';
+import Login from './routes/Login';
+import CreateAccount from './routes/CreateAccount';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/create-account',
+    element: <CreateAccount />,
+  },
+]);
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 
 export default App;
