3f68c7e839da56af193d1fa0bad90e82d4bfa99b
Jaeeun <jaeeun9862@gmail.com>
2023-11-08 11:43:30 +0900

    로그인 페이지 기능 추가

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,5 +1,95 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { FirebaseError } from 'firebase/app';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase';
+import {
+  Error,
+  Form,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+} from '../components/AuthComponents';
+
+interface ErrorMsg {
+  [code: string]: string;
+}
+
+const errorMsg: ErrorMsg = {
+  'auth/invalid-login-credentials': '잘못된 로그인 정보입니다',
+  'auth/user-not-found': '일치하는 사용자를 찾을 수 없습니다.',
+  'auth/wrong-password': '비밀번호가 일치하지 않습니다.',
+  'too-many-requests': '요청 수가 초과하였습니다. 관리자에게 문의하세요.',
+};
+
 const Login = () => {
-  return <h1>Login</h1>;
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setError('');
+    if (loading || email === '' || password === '') return;
+    try {
+      setLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      // redirect to the home page
+      navigate('/');
+    } catch (e) {
+      // setError
+      if (e instanceof FirebaseError) {
+        setError(errorMsg[e.code]);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Title>Log into 🕊️</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name='email'
+          value={email}
+          placeholder='Email'
+          type='email'
+          required
+        />
+        <Input
+          onChange={onChange}
+          name='password'
+          value={password}
+          placeholder='Password'
+          type='password'
+          required
+        />
+        <Input type='submit' value={loading ? 'loading...' : 'Login'} />
+      </Form>
+      {error !== '' ? <Error>{error}</Error> : null}
+      <Switcher>
+        계정이 없으신가요? <Link to='/create-account'>Create →</Link>
+      </Switcher>
+    </Wrapper>
+  );
 };
 
 export default Login;
