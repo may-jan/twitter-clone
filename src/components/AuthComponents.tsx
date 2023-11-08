450932368c2c7412053ec939a2ed12687f9d3abd
Jaeeun <jaeeun9862@gmail.com>
2023-11-08 11:43:55 +0900

    로그인 + 계정 생성 페이지 공통 스타일

---
+++ src/components/AuthComponents.tsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  width: 420px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px 0px;
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+`;
+
+export const Form = styled.form`
+  width: 100%;
+  margin-top: 50px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: none;
+  box-sizing: border-box;
+  font-size: 16px;
+  &[type='submit'] {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+
+export const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+`;
+
+export const Switcher = styled.span`
+  margin-top: 20px;
+  a {
+    color: #1d9bf0;
+  }
+`;
