6f3fdbe35bb4acc1e1c9d30780f7a76b9cd020ab
Jaeeun <jaeeun9862@gmail.com>
2023-11-09 10:46:28 +0900

    GithubBtn + GooleBtn 공통 스타일

---
+++ src/components/SocialComponents.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Button = styled.span`
+  margin-top: 10px;
+  width: 100%;
+  background-color: #fff;
+  font-weight: 500;
+  color: #000;
+  padding: 10px 20px;
+  border-radius: 50px;
+  border: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  cursor: pointer;
+`;
+
+export const Logo = styled.img`
+  height: 25px;
+`;
