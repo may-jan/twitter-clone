62f2df757fb500f1040e5eb54a329988fec9943e
Jaeeun <jaeeun9862@gmail.com>
2023-11-11 22:57:21 +0900

    Form 제출시 첨부한 File 정보 전송, File 크기 제한

--- src/components/PostTweetForm.tsx
+++ src/components/PostTweetForm.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import { useState } from 'react';
-import { addDoc, collection } from 'firebase/firestore';
-import { auth, db } from '../firebase';
+import { addDoc, collection, updateDoc } from 'firebase/firestore';
+import { auth, db, storage } from '../firebase';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 const Form = styled.form`
   display: flex;
@@ -73,7 +74,7 @@ const PostTweetForm = () => {
 
   const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { files } = e.target;
-    if (files && files.length === 1) {
+    if (files && files.length === 1 && files[0].size < 1000000) {
       setFile(files[0]);
     }
   };
@@ -86,13 +87,29 @@ const PostTweetForm = () => {
 
     try {
       setLoading(true);
-      await addDoc(collection(db, 'tweets'), {
+      const doc = await addDoc(collection(db, 'tweets'), {
         tweet,
         createdAt: Date.now(),
         username: user.displayName || 'Anonymous',
         userId: user.uid,
       });
+
+      // 업로드한 파일이 있는 경우
+      if (file) {
+        // Cloud Storage 루트 지정
+        const locationRef = ref(
+          storage,
+          `tweets/${user.uid}-${user.displayName}/${doc.id}`
+        );
+        // Firebase에 업로드 (https://firebase.google.com/docs/storage/web/upload-files?hl=ko#upload_files)
+        const result = await uploadBytes(locationRef, file);
+        // 파일의 다운로드 URL 정보 가져오기 (https://firebase.google.com/docs/storage/web/download-files?hl=ko#download_data_via_url)
+        const url = await getDownloadURL(result.ref);
+        // 기존 doc에 다운로드한 URL 추가해 업데이트 하기 (https://firebase.google.com/docs/firestore/manage-data/add-data?hl=ko#update-data)
+        updateDoc(doc, { photo: url });
+      }
       setTweet('');
+      setFile(null);
     } catch (e) {
       console.log(e);
     } finally {
