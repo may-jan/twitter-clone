53aeb4a8049d81cd0436a675201e916109e2ac26
jaeeun <jaeeun9862@gmail.com>
2023-11-21 23:23:25 +0900

    favicon 적용 방식 변경으로 불필요한 코드 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { ViteFaviconsPlugin } from 'vite-plugin-favicon';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteFaviconsPlugin({
-      logo: 'public/logo.png',
-    }),
-  ],
+  plugins: [react()],
 });
