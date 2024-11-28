399a8e5560a53133ae9b36c79b5044b305a2f349
jaeeun <jaeeun9862@gmail.com>
2024-11-28 22:46:48 +0900

    firebase 배포 오류 수정

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,13 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAfNFYch40fZCyqUMnQPSI0buTNQHWgc34',
-  authDomain: 'twitter-clone-ecfec.firebaseapp.com',
-  projectId: 'twitter-clone-ecfec',
-  storageBucket: 'twitter-clone-ecfec.appspot.com',
-  messagingSenderId: '1045931875438',
-  appId: '1:1045931875438:web:ad5df476db4346eab55a7f',
+  apiKey: 'AIzaSyBQeG6qZTj1bt1QGesp5IxxOU_paII1PI0',
+  authDomain: 'twitter-clone-e6138.firebaseapp.com',
+  projectId: 'twitter-clone-e6138',
+  storageBucket: 'twitter-clone-e6138.firebasestorage.app',
+  messagingSenderId: '304421986476',
+  appId: '1:304421986476:web:e8c04d306973ef058d3f09',
+  measurementId: 'G-WMGQTZ6N7M',
 };
 
 const app = initializeApp(firebaseConfig);
